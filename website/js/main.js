2bf7207818bc5f479236f98d62eb5eed71fd72fb
Chelsea Li <chenyaoli19@gmail.com>
2016-09-18 14:24:45 -0700

    added full page project

---
+++ website/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		// sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
+		anchors: ['1998', '2003', '2008', '2013', '2018'],
+		menu: '#menu',
+		scrollingSpeed: 1000
+	});
+});
